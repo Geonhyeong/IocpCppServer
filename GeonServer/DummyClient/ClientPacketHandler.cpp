26f62022895c09f78710f4d6675822bc6183e561
Geonhyeong <42195466+Geonhyeong@users.noreply.github.com>
2022-09-10 20:32:43 +0900

    패킷 직렬화 #1
    파일을 입출력 할 때나 네트워크 통신을 할 때, 객체를 있는 그대로 보내게 되면 크래쉬가 난다.
    왜냐하면 포인터나 배열 등과 같이 주소값들은 그대로 저장하거나 클라이언트에게 보내봤자 다시 불러오거나 클라이언트에서 읽을 때 엉뚱한 주소를 참조하게 되기 때문이다. 그래서 패킷 직렬화를 해야한다.
    
    먼저 직렬화하는 첫 번째 아이디어로 고정적인 데이터를 먼저 넣고 가변 데이터의 정보를 나타내는 헤더값을 offset과 count로 넣어줌.
    List 안의 List 같은 경우는 아직 고려하지 않음. 구글 FlatBuffers가 만들어지는 원리이기도 함.
    
    이 방법을 통해 고정적인 부분이 앞에 오고 가변적인 부분은 뒤에 오는데 offset으로 해당 데이터 위치를 따로 들고 있기 때문에 Validation을 바로 해줄 수 있다. 즉, 파싱이 빨라짐.

--- GeonServer/DummyClient/ClientPacketHandler.cpp
+++ GeonServer/DummyClient/ClientPacketHandler.cpp
@@ -18,62 +18,70 @@ void ClientPacketHandler::HandlePacket(BYTE* buffer, int32 len)
 }
 
 // 패킷 설계 TEMP
-struct BuffData
+// 메모리에 저장할 때 자동으로 가장 큰 바이트 단위로 맞추어 더미 값을 넣는데 이를 1로 고정한다.
+#pragma pack(1)
+// [PKT_S_TEST][BuffsListItem BuffsListItem BuffsListItem]
+struct PKT_S_TEST
 {
-	uint64 buffId;
-	float remainTime;
-};
+	struct BuffsListItem
+	{
+		uint64 buffId;
+		float remainTime;
+	};
 
-struct S_TEST
-{
-	uint64 id;
-	uint32 hp;
-	uint16 attack;
-	// 가변 데이터
-	// 1) 문자열 (ex. name)
-	// 2) 그냥 바이트 배열 (ex. 길드 이미지)
-	// 3) 일반 리스트
-	vector<BuffData> buffs;
-	wstring name;
+	uint16 packetSize; // 공용 헤더
+	uint16 packetId; // 공용 헤더
+	uint64 id; // 8
+	uint32 hp; // 4
+	uint16 attack; // 2
+	uint16 buffsOffset;
+	uint16 buffsCount;
+
+	bool Validate()
+	{
+		uint32 size = 0;
+
+		size += sizeof(PKT_S_TEST);
+		size += buffsCount * sizeof(BuffsListItem);
+
+		if (size != packetSize)
+			return false;
+
+		if (buffsOffset + buffsCount * sizeof(BuffsListItem) > packetSize)
+			return false;
+		
+		return true;
+	}
+
+	//vector<BuffData> buffs;
+	//wstring name;
 };
+#pragma pack()
 
 void ClientPacketHandler::Handle_S_TEST(BYTE* buffer, int32 len)
 {
 	BufferReader br(buffer, len);
 
-	PacketHeader header;
-	br >> header;
+	if (len < sizeof(PKT_S_TEST))
+		return;
 
-	uint64 id;
-	uint32 hp;
-	uint16 attack;
-	br >> id >> hp >> attack;
+	PKT_S_TEST pkt;
+	br >> pkt;
 
-	cout << "ID : " << id << " HP : " << hp << " ATT : " << attack << endl;
+	if (pkt.Validate() == false)
+		return;
 
-	vector<BuffData> buffs;
-	uint16 buffCount;
-	br >> buffCount;
+	//cout << "ID : " << id << " HP : " << hp << " ATT : " << attack << endl;
 
-	buffs.resize(buffCount);
-	for (int32 i = 0; i < buffCount; i++)
-	{
-		br >> buffs[i].buffId >> buffs[i].remainTime;
-	}
+	vector<PKT_S_TEST::BuffsListItem> buffs;
 
-	cout << "BuffCount : " << buffCount << endl;
-	for (int32 i = 0; i < buffCount; i++)
+	buffs.resize(pkt.buffsCount);
+	for (int32 i = 0; i < pkt.buffsCount; i++)
+		br >> buffs[i];
+
+	cout << "BuffCount : " << pkt.buffsCount << endl;
+	for (int32 i = 0; i < pkt.buffsCount; i++)
 	{
 		cout << "Buff Info : " << buffs[i].buffId << " " << buffs[i].remainTime << endl;
 	}
-
-	wstring name;
-	uint16 nameLen;
-	br >> nameLen;
-	name.resize(nameLen);
-
-	br.Read((void*)name.data(), nameLen * sizeof(WCHAR));
-
-	wcout.imbue(std::locale("kor"));
-	wcout << name << endl;
 }
