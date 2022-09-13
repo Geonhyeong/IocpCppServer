64df08f8dbffd032c81e88b30e84d1ccc6c0fac8
Geonhyeong <42195466+Geonhyeong@users.noreply.github.com>
2022-09-13 21:51:04 +0900

    Protobuf
    구글에서 제공하는 버퍼 API인 Protocol buffers; Protobuf를 사용하기 위한 환경 셋팅과 Protobuf를 사용하여 패킷 생성 및 파싱.
    (강의에서는 3.17버젼을 나는 3.21버젼을 사용함)
    
    C++뿐만 아니라 C# 등 여러 언어를 지원함. 그리고 리니지M과 같은 유명한 프로젝트들도 Protobuf를 채택하여 개발하였다.
    
    살짝 아쉬운 점은 패킷을 하나 만들 때마다 함수를 연동하는 부분을 손수 만들어야 하는데 이를 자동화하여 반복적인 작업을 줄이도록 한다.

--- GeonServer/DummyClient/ClientPacketHandler.cpp
+++ GeonServer/DummyClient/ClientPacketHandler.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "ClientPacketHandler.h"
 #include "BufferReader.h"
+#include "Protocol.pb.h"
 
 void ClientPacketHandler::HandlePacket(BYTE* buffer, int32 len)
 {
@@ -17,86 +18,25 @@ void ClientPacketHandler::HandlePacket(BYTE* buffer, int32 len)
 	}
 }
 
-// 패킷 설계 TEMP
-// 메모리에 저장할 때 자동으로 가장 큰 바이트 단위로 맞추어 더미 값을 넣는데 이를 1로 고정한다.
-#pragma pack(1)
-// [PKT_S_TEST][BuffsListItem BuffsListItem BuffsListItem]
-struct PKT_S_TEST
-{
-	struct BuffsListItem
-	{
-		uint64 buffId;
-		float remainTime;
-	};
-
-	uint16 packetSize; // 공용 헤더
-	uint16 packetId; // 공용 헤더
-	uint64 id; // 8
-	uint32 hp; // 4
-	uint16 attack; // 2
-	uint16 buffsOffset;
-	uint16 buffsCount;
-
-	bool Validate()
-	{
-		uint32 size = 0;
-		size += sizeof(PKT_S_TEST);
-		if (packetSize < size)
-			return false;
-
-		size += buffsCount * sizeof(BuffsListItem);
-
-		if (size != packetSize)
-			return false;
-
-		if (buffsOffset + buffsCount * sizeof(BuffsListItem) > packetSize)
-			return false;
-		
-		return true;
-	}
-
-	using BuffsList = PacketList<PKT_S_TEST::BuffsListItem>;
-
-	BuffsList GetBuffList()
-	{
-		BYTE* data = reinterpret_cast<BYTE*>(this);
-		data += buffsOffset;
-		return BuffsList(reinterpret_cast<PKT_S_TEST::BuffsListItem*>(data), buffsCount);
-	}
-	//vector<BuffData> buffs;
-	//wstring name;
-};
-#pragma pack()
-
 void ClientPacketHandler::Handle_S_TEST(BYTE* buffer, int32 len)
 {
-	BufferReader br(buffer, len);
+	Protocol::S_TEST pkt;
 
-	if (len < sizeof(PKT_S_TEST))
-		return;
+	ASSERT_CRASH(pkt.ParseFromArray(buffer + sizeof(PacketHeader), len - sizeof(PacketHeader)));
 
-	PKT_S_TEST* pkt = reinterpret_cast<PKT_S_TEST*>(buffer);
+	cout << pkt.id() << " " << pkt.hp() << " " << pkt.attack() << endl;
 
-	if (pkt->Validate() == false)
-		return;
+	cout << "BUFSIZE : " << pkt.buffs_size() << endl;
 
-	//cout << "ID : " << id << " HP : " << hp << " ATT : " << attack << endl;
-
-	PKT_S_TEST::BuffsList buffs = pkt->GetBuffList();
-	
-	cout << "BuffCount : " << buffs.Count() << endl;
-	for (int32 i = 0; i < buffs.Count(); i++)
+	for (auto& buf : pkt.buffs())
 	{
-		cout << "Buff Info : " << buffs[i].buffId << " " << buffs[i].remainTime << endl;
-	}
+		cout << "BUFINFO : " << buf.buffid() << " " << buf.remaintime() << endl;
+		cout << "VICTIMS : " << buf.victims_size() << endl;
+		for (auto& vic : buf.victims())
+		{
+			cout << vic << "  ";
+		}
 
-	for (auto it = buffs.begin(); it != buffs.end(); ++it)
-	{
-		cout << "Buff Info : " << it->buffId << " " << it->remainTime << endl;
-	}
-
-	for (auto& buff : buffs)
-	{
-		cout << "Buff Info : " << buff.buffId << " " << buff.remainTime << endl;
+		cout << endl;
 	}
 }
